2a08d60c541a7b247590888de30566733233cf4b
Maksym Makarychev <m.v.makarychev@gmail.com>
2018-01-23 22:25:11 +0200

    fix: body msg encoding

--- hello.py
+++ hello.py
@@ -1,20 +1,23 @@
-
 def parse_qs(query_string):
     result = ""
     if query_string:
         list_res = query_string.split('&')
-        result = '\n'.join(list_res)
+        result = [bytes(i+'\n', 'ascii') for i in list_res]
     return result
 
 def wsgi_application(environ, start_response):
-    # print("===*** wsgi_app START ***===")
-    # print(environ['PATH_INFO'])
+    print("===*** wsgi_app START ***===")
+    print(environ['PATH_INFO'])
+    print(environ['QUERY_STRING'])
     body = parse_qs(environ['QUERY_STRING']) #todo: parse request
     status = '200 OK'
+    content_length = sum(len(s) for s in body)
     headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(body)))
+       ('Content-Length', str(content_length))
     ]
-    # print("response body: " + body)
+    print("response body: " + str(body))
+    print("body len: " + str(content_length))
     start_response(status, headers)
-    return [body]
+
+    return body
